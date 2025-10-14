3b197dd8f09c747e877b6abe984730882008cfb1
Joe Burgess <joeezmail@gmail.com>
2025-10-14 00:29:47 -0700

    Create Game class, implement battle() that handles battle loop, implement  display_status()

--- main.py
+++ main.py
@@ -1,52 +1,103 @@
 import random
 import os
 import classes
-from classes import Warrior, Mob, Char, Items
 
-# new_toon = classes.Warrior("bob")
-# new_mob = classes.Mob("goblin")
-new_toon = Warrior("bob")
-new_mob = Mob("goblin")
+
+class Game:
+    def __init__(self, player, mob):
+        self.player = player
+        self.mob = mob
+
+    def battle(self, player, mob):
+        """
+        Handles battle loop
+        """
+        # clear the console
+        os.system("cls" if os.name == "nt" else "clear")
+        print(f"You encounter a {mob.name}!")
+        input("Press enter to continue...")
+        print("")
+
+        while player.health > 0 and mob.health > 0:
+            # os.system("cls" if os.name == "nt" else "clear")
+            self.display_status(player, mob)
+            print("")
+            player.do_action(mob)
+            if mob.health <= 0:
+                print(f"You have defeated the {mob.name}!")
+                player.loot_mob(mob)
+                print(f"Inventory: {player.inventory}")
+                return
+                # break
+
+            mob.do_action(player)
+            if player.health <= 0:
+                print("You have been defeated!")
+                return
+                # break
+
+    def display_status(self, player, mob):
+        print("--------- Toon Status ---------")
+        print(f"{new_toon.name} Health: {new_toon.health}")
+        print(f"Attack Power: {new_toon.atk_power}")
+        print(f"Armor Class: {new_toon.armor_class}")
+        print(f"Weapon: {new_toon.weapon_name}")
+        print(f"weapon Damage: {new_toon.weapon_damage}")
+        print(f"Enrage Counter: {new_toon.enrage_cooldown}")
+        print(f"Inventory: {new_toon.inventory}")
+        print("---------------------------")
+        print("")
+        print("--------- Mob Status ---------")
+        print(f"{new_mob.name} Health: {new_mob.health}")
+        print(f"Attack Power: {new_mob.atk_power}")
+        print(f"Armor Class: {new_mob.armor_class}")
+        print(f"Weapon: {new_mob.weapon_name}")
+        print(f"weapon Damage: {new_mob.weapon_damage}")
+        print(f"Inventory: {new_mob.inventory}")
 
 
 #### TESTING AREA CODE #######
+new_toon = classes.Warrior("bob")
+new_mob = classes.Mob("goblin")
+game = Game(new_toon, new_mob)
+game.battle(new_toon, new_mob)
 # new_toon = Warrior("bob")
 # new_mob = Mob("goblin")
 
-print(f"You encounter a {new_mob.name}!")
-# input("Press enter to continue...")
-print("")
-
-while new_toon.health > 0 and new_mob.health > 0:
-    print("--------- Toon Status ---------")
-    print(f"{new_toon.name} Health: {new_toon.health}")
-    print(f"{new_mob.name} Health: {new_mob.health}")
-    print(f"Attack Power: {new_toon.atk_power}")
-    print(f"Armor Class: {new_toon.armor_class}")
-    print(f"Weapon: {new_toon.weapon_name}")
-    print(f"weapon Damage: {new_toon.weapon_damage}")
-    print(f"Enrage Counter: {new_toon.enrage_cooldown}")
-    print(f"Inventory: {new_toon.inventory}")
-    print("---------------------------")
-    print("")
-    print("--------- Mob Status ---------")
-    print(f"{new_mob.name} Health: {new_mob.health}")
-    print(f"Attack Power: {new_mob.atk_power}")
-    print(f"Armor Class: {new_mob.armor_class}")
-    print(f"Weapon: {new_mob.weapon_name}")
-    print(f"weapon Damage: {new_mob.weapon_damage}")
-    #
-    #     ## Attack and check if mob or player is stuned or dead
-    # new_toon.do_action(new_mob.name, new_mob.armor_class, new_toon.atk_power)
-    new_toon.do_action(new_mob)
-    if new_mob.health <= 0:
-        print(f"You have defeated the {new_mob.name}!")
-        new_toon.loot_mob(new_mob)
-        print(f"Inventory: {new_toon.inventory}")
-        break
-
-    # new_mob.do_action(new_toon.name, new_toon.armor_class, new_mob.atk_power)
-    new_mob.do_action(new_toon)
-    if new_toon.health <= 0:
-        print("You have been defeated!")
-        break
+# print(f"You encounter a {new_mob.name}!")
+# # input("Press enter to continue...")
+# print("")
+#
+# while new_toon.health > 0 and new_mob.health > 0:
+#     print("--------- Toon Status ---------")
+#     print(f"{new_toon.name} Health: {new_toon.health}")
+#     print(f"Attack Power: {new_toon.atk_power}")
+#     print(f"Armor Class: {new_toon.armor_class}")
+#     print(f"Weapon: {new_toon.weapon_name}")
+#     print(f"weapon Damage: {new_toon.weapon_damage}")
+#     print(f"Enrage Counter: {new_toon.enrage_cooldown}")
+#     print(f"Inventory: {new_toon.inventory}")
+#     print("---------------------------")
+#     print("")
+#     print("--------- Mob Status ---------")
+#     print(f"{new_mob.name} Health: {new_mob.health}")
+#     print(f"Attack Power: {new_mob.atk_power}")
+#     print(f"Armor Class: {new_mob.armor_class}")
+#     print(f"Weapon: {new_mob.weapon_name}")
+#     print(f"weapon Damage: {new_mob.weapon_damage}")
+#     print(f"Inventory: {new_mob.inventory}")
+#     #
+#     #     ## Attack and check if mob or player is stuned or dead
+#     # new_toon.do_action(new_mob.name, new_mob.armor_class, new_toon.atk_power)
+#     new_toon.do_action(new_mob)
+#     if new_mob.health <= 0:
+#         print(f"You have defeated the {new_mob.name}!")
+#         new_toon.loot_mob(new_mob)
+#         print(f"Inventory: {new_toon.inventory}")
+#         break
+#
+#     # new_mob.do_action(new_toon.name, new_toon.armor_class, new_mob.atk_power)
+#     new_mob.do_action(new_toon)
+#     if new_toon.health <= 0:
+#         print("You have been defeated!")
+#         break
